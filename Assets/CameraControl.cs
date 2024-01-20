2293a6e93edb9518fbd110e2cb4409f335fdb356
mikulickaaa <katarina.mikulic@fer.hr>
2024-01-20 19:46:42 +0100

    Fix rotation camera

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public float brzinaRotacije = 5.0f;
+    public float maksimalnaRotacijaX = 15f;
+    public float minimalnaRotacijaX = -15f;
+    public float maksimalnaRotacijaY = 15f;
+    public float minimalnaRotacijaY = -15f;
+
+    private float trenutnaRotacijaX = 0f;
+    private float trenutnaRotacijaY = 0f;
 
     void Update()
     {
@@ -20,8 +27,17 @@ public class CameraControl : MonoBehaviour
             unosHorizontalnoKamere = 1;
 
         Vector3 rotacijaKamere = new Vector3(unosVerticalnoKamere, unosHorizontalnoKamere, 0) * brzinaRotacije * Time.deltaTime;
-        transform.Rotate(rotacijaKamere);
 
+        // Prati trenutnu rotaciju
+        trenutnaRotacijaX += rotacijaKamere.x;
+        trenutnaRotacijaY += rotacijaKamere.y;
+
+        // Linearna interpolacija izmeðu minimalne i maksimalne rotacije
+        trenutnaRotacijaX = Mathf.Lerp(trenutnaRotacijaX, Mathf.Clamp(trenutnaRotacijaX, minimalnaRotacijaX, maksimalnaRotacijaX), Time.deltaTime * 2f);
+        trenutnaRotacijaY = Mathf.Lerp(trenutnaRotacijaY, Mathf.Clamp(trenutnaRotacijaY, minimalnaRotacijaY, maksimalnaRotacijaY), Time.deltaTime * 2f);
+
+        // Rotiraj kameru oko svojih lokalnih osi
+        transform.localRotation = Quaternion.Euler(trenutnaRotacijaX, trenutnaRotacijaY, 0);
 
     }
 }
